ef9032689f33e95efb65aac10bffbc83c1f23d18
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2023-01-10 19:18:57 +0100

    Train image

---
+++ app.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget
+from PyQt6.QtGui import QPixmap, QIcon
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+ 
+        self.setGeometry(200, 200, 700, 400)
+        self.setWindowTitle("TgoesWrum")
+        self.setWindowIcon(QIcon('./static/images/train.png'))
+ 
+        label = QLabel(self)
+        pixmap = QPixmap('./static/images/train.png')
+        label.setPixmap(pixmap)
+ 
+ 
+ 
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
